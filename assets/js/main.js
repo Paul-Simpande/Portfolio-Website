178a4be14cc1317a9d8a5fef8964bb1356a91570
Paul Simpande <simpandepaul03@gmail.com>
2024-11-23 17:45:56 +0200

    added active link code

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,8 +53,8 @@ const sendEmail = (e) =>{
             contactForm.reset()
         })
         .catch((error) => {
-            // Handle errors
-            contactMessage.textContent = 'Failed to send message. Please try again ❌';
+            // Handle errors and shows if message is not sent
+            contactMessage.textContent = 'Failed to send message. (Service error) ❌';
             console.error('Error:', error);
 
             // Remove message after 5 seconds
@@ -66,9 +66,34 @@ const sendEmail = (e) =>{
 contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
