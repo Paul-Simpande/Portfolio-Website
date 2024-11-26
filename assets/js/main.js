1e0bf00982d7916c1cce138c611538c28a1d91f8
Paul Simpande <simpandepaul03@gmail.com>
2024-11-26 12:25:20 +0200

    Added glowing particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,62 @@ sr.reveal(`.home__scroll`, {delay: 800})
 sr.reveal(`.work__card, .services__card`, {interval: 100})
 sr.reveal(`.about__content`, {origin: 'right'})
 sr.reveal(`.about__img`, {origin: 'left'})
+
+const canvas = document.getElementById('particleCanvas');
+const ctx = canvas.getContext('2d');
+
+// Resize canvas to fit screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Particle properties
+const particles = [];
+const particleCount = 100;
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = Math.random() * 3;
+        this.speedX = (Math.random() - 0.5) * 2;
+        this.speedY = (Math.random() - 0.5) * 2;
+        this.color = `rgba(255, 255, ${Math.random() * 255}, 0.8)`;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // Bounce off edges
+        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+// Initialize particles
+for (let i = 0; i < particleCount; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    particles.push(new Particle(x, y));
+}
+
+// Animation loop
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((particle) => {
+        particle.update();
+        particle.draw();
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
